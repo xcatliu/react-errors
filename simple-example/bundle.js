cca16a09839c3ea27c57f1322383c5e8fa5037df
xcatliu <xcatliu@gmail.com>
2016-01-21 20:10:26 +0800

    Update gh-pages

--- simple-example/bundle.js
+++ simple-example/bundle.js
@@ -19825,12 +19825,12 @@
 	 * Errors Component
 	 *
 	 * Warning: This component will render exactly what messages in `errors`,
-	 * so you should change `errors` when `onErrorClose` is triggered.
+	 * so you should change `errors` whenever `onErrorClose` triggered.
 	 *
 	 * @props   errors        array of error, each error should be a instance of
 	 *                        `Error` or an object which contains `message`
-	 * @props   onErrorClose  fire when close button clicked, with a param which is
-	 *                        the index of errors
+	 * @props   onErrorClose  trigger when the close button is clicked, with a param
+	 *                        which is the index of errors
 	 */
 	
 	var Errors = function (_Component) {
