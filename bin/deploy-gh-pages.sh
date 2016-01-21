de270a80a0f168b3be53209485ac695e3b9fbe0c
xcatliu <xcatliu@gmail.com>
2016-01-21 19:43:12 +0800

    Update deploy shell
    Update deploy shell
    
    Update deploy shell

--- bin/deploy-gh-pages.sh
+++ bin/deploy-gh-pages.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 set -e
 
-# clone react-errors repo
-rm -rf react-errors
-git clone git@github.com:xcatliu/react-errors.git react-errors
-
-cd react-errors
 git checkout gh-pages
+git pull origin gh-pages
 
 git checkout master examples
 
-mv examples/* ./
+# http://askubuntu.com/questions/269775/mv-directory-not-empty
+rsync -a examples/ ./
 rm -rf examples
 
 git add :
 git commit -m 'Update gh-pages'
 git push origin gh-pages
 
-cd ..
-rm -rf react-errors
+git checkout master
